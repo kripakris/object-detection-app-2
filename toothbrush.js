381b405208f1697c73af3b8803e5712b9e3ceb10
kripakris <98467708+kripakris@users.noreply.github.com>
2022-10-26 19:05:30 +0530

    Add files via upload

---
+++ toothbrush.js
@@ -0,0 +1,51 @@
+img = "";
+status ="";
+object = [];
+function preload(){
+    img = loadImage("toothbrush.jpg")
+}
+
+function setup()
+{
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : detecting objects";
+}
+
+function modelLoaded() {
+    console.log("model loaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+
+function gotResult(error , results){
+if (error) {
+    console.log(error);
+}
+console.log(results);
+
+object = results;
+}
+
+function draw()
+{
+    image(img , 0,0, 640,420);
+
+    if(status != "")
+    {
+        for (i=0;i< object.length;i++){
+            document.getElementById("status").innerHTML = "Status : object detected";
+
+        fill("purple");
+        percent = floor(object[i].confidence*100);
+        text(object[i].label + " " + percent + "%" , object[i].x, object[i].y);
+        noFill();
+        stroke("purple");
+        rect(object[i].x , object[i].y , object[i].width , object[i].height);
+
+        }
+    }
+
+}
